e13a32bc7f3ebcc4e4ffdf62ecd996354212cc80
alisson <alissongouvei@gmail.com>
2019-05-14 10:50:39 -0400

    up

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="sortcut icon" href="public/favicon.ico" type="image/x-icon" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Eagle | Music Videos </title>
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	Creative Desing
+	DESIGNED & DEVELOPED by wcDesign.com
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  <!-- Animate.css -->
+  <link rel="stylesheet" href="public/vendors/css/animate.css">
+  <!-- Icomoon Icon Fonts-->
+  <link rel="stylesheet" href="public/vendors/css/icomoon.css">
+  <!-- Simple Line Icons-->
+  <link rel="stylesheet" href="public/vendors/css/simple-line-icons.css">
+  <!-- Magnific Popup -->
+  <link rel="stylesheet" href="public/vendors/css/magnific-popup.css">
+  <!-- Owl Carousel -->
+  <link rel="stylesheet" href="public/vendors/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="public/vendors/css/owl.theme.default.min.css">
+  <!-- Salvattore -->
+  <link rel="stylesheet" href="public/vendors/css/salvattore.css">
+  <!-- Theme Style -->
+  <link rel="stylesheet" href="public/vendors/css/style.css">
+  <!-- Modernizr JS -->
+  <script src="public/vendors/js/modernizr-2.6.2.min.js"></script>
+  <!-- FOR IE9 below -->
+
+</head>
+
+<body>
+<div>
+<?php include "public/containers/menu.html";?>
+<?php include "public/containers/quemSomos.html";?>
+<?php include "public/containers/top1.html";?>
+<?php include "public/containers/anuncio.html";?>
+<?php include "public/containers/fotos.html";?>
+<?php include "public/containers/footer.html";?>
+
+
+
+  
+</div>
+
+
+    
+
+  <script type="text/javascript" src="public/vendors/js/jquery.min.js"></script>
+  <script src="public/vendors/bootstrap/js/bootstrap.js"></script>
+
+  <script>
+    $(function () {
+      $("#head-title").typed({
+        strings: ["Sejá muito Bem Vindo !!!^50", "Noś somos há^50", "Eagle Music^50",],
+        typeSpeed: 100,
+        loop: true,
+        startDelay: 100
+      });
+    });
+  </script>
+
+  <!-- <div class="content">
+        <h4>We've got the special power</h4>
+        <hr>
+        <div class="header-text btn">
+            <h1><span id="head-title"></span></h1>
+        </div>
+      </div>
+       -->
+
+  <script type="text/javascript" src="public/vendors/js/jquery.easing.1.3.js"></script>
+
+  <script type="text/javascript" src="public/vendors/js/typed.js"></script>
+
+  <script type="text/javascript" src="public/vendors/js/jquery.waypoints.min.js"></script>
+  <!-- Magnific Popup -->
+  <script type="text/javascript" src="public/vendors/js/jquery.magnific-popup.min.js"></script>
+  <!-- Owl Carousel -->
+  <script type="text/javascript" src="public/vendors/js/owl.carousel.min.js"></script>
+  <!-- toCount -->
+  <script type="text/javascript" src="public/vendors/js/jquery.countTo.js"></script>
+  <!-- Main JS -->
+  <script type="text/javascript" src="public/vendors/js/main.js"></script>
+
+
+
+
+</body>
+
+</html>
